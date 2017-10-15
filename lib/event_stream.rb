4d56b3a8682e590740d177a7281f38602431041c
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-10-15 20:11:25 +0100

    Syntax error

--- lib/event_stream.rb
+++ lib/event_stream.rb
@@ -3,7 +3,7 @@ require 'oj'
 class EventStream
   class << self
     def event_type(event)
-      event.class.name..gsub(/::/, '/').
+      event.class.name.gsub(/::/, '/').
         gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
         gsub(/([a-z\d])([A-Z])/,'\1_\2').
         tr("-", "_").
